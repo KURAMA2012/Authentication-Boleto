39af694de5f1870e689c40b0dc88a6064d3e2f2b
Davy Lucas <davykog2012@gmail.com>
2025-11-12 12:30:02 -0300

    removendo imagens

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
+
   { path: '', redirectTo: 'boleto/list', pathMatch: 'full' },
 
-  //{ path: 'empresa/list', loadChildren: () => import('./pages/empresa/empresa-list/empresa-list.module').then(m => m.EmpresaListPageModule) },
-  //{ path: 'empresa/form', loadChildren: () => import('./pages/empresa/empresa-form/empresa-form.module').then(m => m.EmpresaFormPageModule) },
-
-  //{ path: 'boleto/list', loadChildren: () => import('./pages/boleto/boleto-list/boleto-list.module').then(m => m.BoletoListPageModule) },
-  //{ path: 'boleto/detalhe/:id', loadChildren: () => import('./pages/boleto/boleto-detalhe/boleto-detalhe.module').then(m => m.BoletoDetalhePageModule) },
   { path: 'boleto/validacao', loadChildren: () => import('./pages/boleto/boleto-validacao/boleto-validacao.module').then(m => m.BoletoValidacaoPageModule) },
 
-  //{ path: 'token/validacao', loadChildren: () => import('./pages/token/token-validacao/token-validacao.module').then(m => m.TokenValidacaoPageModule) },
-
-  //{ path: 'logs', loadChildren: () => import('./pages/logs/boleto-logs/boleto-logs.module').then(m => m.BoletoLogsPageModule) },
   {
     path: 'boleto-validacao',
     loadChildren: () => import('./pages/boleto/boleto-validacao/boleto-validacao.module').then( m => m.BoletoValidacaoPageModule)
@@ -29,7 +25,8 @@ const routes: Routes = [
   {
     path: 'confirmar-token',
     loadChildren: () => import('./pages/token/confirmar-token/confirmar-token.module').then( m => m.ConfirmarTokenPageModule)
-  },  {
+  },
+  {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
@@ -41,7 +38,6 @@ const routes: Routes = [
     path: 'cadastro-empresa',
     loadChildren: () => import('./pages/cadastro-empresa/cadastro-empresa.module').then( m => m.CadastroEmpresaPageModule)
   },
-
 
 ];
 
--- frontend-app/src/app/pages/login/login.page.ts
+++ frontend-app/src/app/pages/login/login.page.ts
@@ -1,194 +1,53 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AlertController, LoadingController, MenuController, NavController, Platform, ToastController } from '@ionic/angular';
-import { BSAuth } from './../../core/services/BSAuth.service';
-import { BSMessage } from './../../core/services/BSMessage.service';
-import { BSAppService } from 'src/app/core/services/BSApp.services';
-import { BSLoading } from 'src/app/core/services/BSLoading.service';
-import { NativeBiometric, BiometryType } from '@capgo/capacitor-native-biometric';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  loginForm: FormGroup;
+  mostrarSenha = false;
 
-  version: string | null = null;
-  onLoginForm: FormGroup;
-  codigoOperador: string | null = null;
-  senha: string | null = null;
-  lembrarUsuario: boolean | null = null;
-
-  inp_statServer: any = null;
-  statusDoServidor: number = 0;
-  bloquearLogin = false;
-
-  cSucessos = 0;
-  cContadorDeErros = 0;
-  pararTimeOut = false;
-
-  @ViewChild("viewLogin") viewLogin: ElementRef;
-
-  constructor(
-    private navCtrl: NavController,
-    private menuCtrl: MenuController,
-    private toastCtrl: ToastController,
-    private alertCtrl: AlertController,
-    private loadingCtrl: LoadingController,
-    private formBuilder: FormBuilder,
-    private bsAuth: BSAuth,
-    private bsMessage: BSMessage,
-    private bsAppRaj: BSAppService,
-    private platform: Platform,
-    private bsApp: BSAppService,
-    private bsLoading: BSLoading
-  ) {}
-
-  ngOnInit() {
-    this.initForm();
-    this.loadSavedCredentials();
-    this.version = this.bsAppRaj.version;
-    this.pararTimeOut = false;
-    this.fluxoTesteDaConexao(3000);
-  }
-
-  ionViewWillEnter() {
-    this.menuCtrl.enable(false);
-    this.atualizaFooterStatusServico();
-  }
-
-  private initForm() {
-    this.onLoginForm = this.formBuilder.group({
-      codigoOperador: [null, Validators.required],
-      senha: [null, Validators.required],
-      lembrarUsuario: [null, Validators.required]
+  constructor(private fb: FormBuilder, private toastCtrl: ToastController) {
+    this.loginForm = this.fb.group({
+      nome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(4)]],
     });
   }
 
-  private loadSavedCredentials() {
-    if (this.bsAppRaj.isLembrarUsuario()) {
-      this.codigoOperador = this.bsAppRaj.getUsuarioLembrar();
-      this.senha = this.bsAppRaj.getSenhaLembrar();
-      this.lembrarUsuario = true;
-    } else {
-      this.codigoOperador = null;
-      this.senha = null;
-      this.lembrarUsuario = false;
-    }
-  }
-
-  scrollIntoView() {
-    // this.viewLogin.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
-  }
-
-  private fluxoTesteDaConexao(nTimeOut: number) {
-    let cTimeOut = 800;
-    setTimeout(() => {
-      this.bsAppRaj.TestarConexao();
-      this.atualizaFooterStatusServico();
-      this.bsAppRaj.LerConfiguracao();
-      this.atualizaFooterStatusServico();
-
-      if (this.bsAppRaj.EstaConectado()) {
-        cTimeOut = 5000;
-        this.cSucessos++;
-        if (this.cSucessos > 5) {
-          this.cSucessos = 0;
-          cTimeOut = 1500;
-          this.bsAppRaj.ForcarNovoTesteDeConexao();
-        }
-      } else if (this.bsAppRaj.EstaComFalhaDeConexaoEmAmbosOsServers()) {
-        cTimeOut = 1500;
-        this.cContadorDeErros++;
-        if (this.cContadorDeErros > 5) {
-          this.cContadorDeErros = 0;
-          this.bsAppRaj.ForcarNovoTesteDeConexao();
-        }
-      } else {
-        cTimeOut = 800;
-      }
-
-      console.log(`${this.cSucessos} - ${this.pararTimeOut} ms ${cTimeOut} - Proximo teste: ${this.bsAppRaj.StatusServerNome()}`);
-
-      if (!this.pararTimeOut) this.fluxoTesteDaConexao(cTimeOut);
-    }, nTimeOut);
-  }
-
-  goToRegister() {
-    this.navCtrl.navigateRoot('/register');
-  }
-
-  async loginTeste() {
-    this.navCtrl.navigateRoot('home-results');
+  toggleSenha() {
+    this.mostrarSenha = !this.mostrarSenha;
   }
 
   async login() {
-    this.pararTimeOut = true;
-    this.bsLoading.loading();
-
-    try {
-      if (this.bsAppRaj.isHabilitarAcessoDigitalConfigurado() && this.bsAppRaj.isHabilitarAcessoDigital()) {
-        await this.logarComDigital();
-      } else {
-        await this.logarRaj();
-      }
-    } finally {
-      this.bsLoading.unloading();
-    }
-  }
-
-  private async logarRaj() {
-    if (!this.codigoOperador || !this.senha) {
-      this.bsMessage.informa("Usuário e Senha devem ser informados");
+    if (this.loginForm.invalid) {
+      const toast = await this.toastCtrl.create({
+        message: 'Preencha todos os campos!',
+        duration: 2000,
+        color: 'warning',
+      });
+      toast.present();
       return;
     }
-    try {
-      await this.bsAuth.autentidarRAJ(this.codigoOperador, this.senha, this.lembrarUsuario);
-    } catch (e) {
-      this.bsMessage.error("Acesso não autorizado");
-    }
+
+    const toast = await this.toastCtrl.create({
+      message: 'Cadastro realizado com sucesso!',
+      duration: 2000,
+      color: 'success',
+    });
+    toast.present();
   }
 
- private async logarComDigital() {
-  try {
-    await this.platform.ready();
-
-    const available = await NativeBiometric.isAvailable();
-
-    // Ajuste para tipos válidos na versão atual
-    if (
-      available.biometryType === BiometryType.FINGERPRINT ||
-      available.biometryType === BiometryType.FACE_AUTHENTICATION
-    ) {
-      await NativeBiometric.verifyIdentity({ reason: 'Autenticação biométrica' });
-
-      await this.bsAuth.autentidarRAJ(
-        this.bsAppRaj.getUsuarioAcessoDigital(),
-        this.bsAppRaj.getSenhaAcessoDigital(),
-        false
-      );
-    } else {
-      await this.logarRaj();
-    }
-
-  } catch (err) {
-    console.error('Erro na autenticação biométrica:', err);
-    await this.logarRaj();
+  async loginRapido() {
+    const toast = await this.toastCtrl.create({
+      message: 'Login realizado com sucesso!',
+      duration: 2000,
+      color: 'success',
+    });
+    toast.present();
   }
-}
-
-  configurar() {
-    this.pararTimeOut = true;
-    this.navCtrl.navigateForward('/configuracao/alterar');
-  }
-
-  atualizaFooterStatusServico() {
-    const stSrv = this.bsAppRaj.getStatusServerInteger();
-    this.inp_statServer = this.bsAppRaj.StatusServerCor();
-    this.statusDoServidor = stSrv;
-    this.bloquearLogin = (stSrv !== 1);
-    console.log(`stSrv ${stSrv} -- ${this.bsAppRaj.StatusServerCor()} -- ${this.bsAppRaj.StatusServerNome()}`);
-  }
-
 }
